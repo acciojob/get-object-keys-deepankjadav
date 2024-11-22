f63651a37532bfd22356c779d886b9b353f3f063
deepankjadav <jadavdeepank@gmail.com>
2024-11-22 22:08:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Creating the student object
+let student = {
+    name: "John Doe"
+};
+
+// Adding getKeys method to the Object prototype
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+// Using the getKeys method on the student object
+let keys = student.getKeys();
+console.log(keys); // Output: ['name']
